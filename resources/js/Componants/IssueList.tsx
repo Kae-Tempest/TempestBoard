c660e140f6217897d65fea57a15d7d7a3cee441e
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-02-23 15:09:32 +0100

    feat/fix : showing modal a + click | Add Tooltip on priority icon | Change Icon

--- resources/js/Componants/IssueList.tsx
+++ resources/js/Componants/IssueList.tsx
@@ -3,8 +3,10 @@ import { issueStateEnum, priorityIconEnum } from "@/enums/global";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CanceledIssueIcon, CompletedIssueIcon, InProgressIssueIcon, OpenIssueIcon } from "@/Componants/StateIcon";
 import { CustomPriorityIcon } from "@/Componants/Priority";
-import { faAnglesDown, faAnglesUp, faChevronDown, faChevronUp, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faAnglesUp, faChevronDown, faCircleExclamation, faMinus, faPause, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import IssueModal from "@/Componants/IssueModal";
+import { useState } from "react";
 
 interface IssueListProps {
     issueArray: Issue[];
@@ -43,26 +45,33 @@ const IssueIcon: React.FC<{ state: string }> = ({ state }) => {
     }
 };
 const PriorityIcon: React.FC<{ priority: string }> = ({ priority }) => {
-    const ListIcon: IconDefinition[] = [faAnglesDown, faChevronDown, faMinus, faChevronUp, faAnglesUp];
+    const ListIcon: IconDefinition[] = [faChevronDown, faMinus, faPause, faAnglesUp, faCircleExclamation];
     const indexOfState = Object.values(priorityIconEnum).indexOf(priority as unknown as priorityIconEnum);
     const key = Object.keys(priorityIconEnum)[indexOfState];
+    let props = 0;
+    if (Number(key) == 2) {
+        props = 90;
+    }
     return (
-        <>
-            <CustomPriorityIcon iconName={ListIcon[Number(key)]} />
-        </>
+        <div className="tooltip">
+            <CustomPriorityIcon iconName={ListIcon[Number(key)]} rotate={props} />
+            <span className="tooltip-text">{priority.toLowerCase()}</span>
+        </div>
     );
 };
 
 export const IssueList: React.FC<IssueListProps> = ({ issueArray, Projects, state }) => {
     const indexOfState = Object.values(issueStateEnum).indexOf(state as unknown as issueStateEnum);
     const key = Object.keys(issueStateEnum)[indexOfState];
+    const [showModal, setShowModal] = useState<boolean>(false);
     return (
         <>
+            <IssueModal projects={Projects} setShowModal={setShowModal} showModal={showModal} />
             <div className="issue-header">
                 <div>
                     <IssueIcon state={state} /> {key}
                 </div>
-                <FontAwesomeIcon icon={faPlus} />
+                <FontAwesomeIcon icon={faPlus} onClick={() => setShowModal(true)} className="issue-add" />
             </div>
             {issueArray
                 .filter(i => i.status === state)
@@ -78,9 +87,6 @@ export const IssueList: React.FC<IssueListProps> = ({ issueArray, Projects, stat
                                             </div>
                                             <div className="tag-number">
                                                 {project.name.substring(0, 3)}-{issue.id}
-                                            </div>
-                                            <div className="state">
-                                                <IssueIcon state={state} />
                                             </div>
                                             <div className="title-issue">{issue.title}</div>
                                         </div>
