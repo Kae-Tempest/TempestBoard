36f89264aec034e7acffaa9c5a5f1032f83fb2b5
KaeTempest <katsuhiko.tempest@gmail.com>
2024-03-09 22:07:34 +0100

    Auth part

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import User, Project, Issue, Role, Tag
 
 
@@ -50,3 +51,22 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'name', 'project', 'created_at', 'updated_at']
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(label="Email", required=True, write_only=True)
+    password = serializers.CharField(style={'input_type': 'password'}, label="Password", write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        if email and password:
+            user = authenticate(request=self.context.get('request'), email=email, password=password)
+            if not user:
+                msg = 'Unable to log in with provided credentials'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = 'Must provide email and password both'
+            raise serializers.ValidationError(msg)
+        attrs['user'] = user
+        return attrs
