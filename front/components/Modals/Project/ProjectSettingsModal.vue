054351b30a8ce95771302526dadd9945823d0f69
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-09-14 19:13:45 +0200

    [Front] Move user add and State add to one modal for project with some little fix

---
+++ front/components/Modals/Project/ProjectSettingsModal.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+import type {Project, States, User} from "~/types/global";
+import {useCustomFetch} from "~/composables/useCustomFetch";
+import CreateStateModal from "~/components/Modals/state/CreateStateModal.vue";
+
+interface Props {
+  project: Project
+}
+
+const props = defineProps<Props>()
+const showModal = defineModel()
+
+const tabs = ref("users")
+
+const users = ref<User[]>([])
+const projectUsers = ref<User[]>([])
+const projectState = ref<States[]>([])
+const openCreateStateModal = ref<Boolean>(false)
+
+const data = reactive({
+  users: [] as string[],
+})
+
+const resetForm = () => {
+  data.users = []
+  users.value = []
+  projectUsers.value = []
+}
+
+const {data: projectStateData, refresh: projectStateRefresh } = await useCustomFetch<States[]>(`/project/${props.project.id}/states/`)
+
+
+const handleFetch = async () => {
+  const {data: pUsers} = await useCustomFetch<User[]>(`/projects/${props.project.id}/users/`)
+  const {data: usersData} = await useCustomFetch<User[]>('/users/')
+  await projectStateRefresh()
+  users.value = usersData.value as User[]
+  projectUsers.value = pUsers.value as User[]
+  projectState.value = projectStateData.value as States[]
+}
+
+watch(() => showModal.value, async (newVal) => {
+  if (newVal) {
+    await handleFetch()
+  }
+})
+
+watch(() => openCreateStateModal.value, async (newVal) => {
+  if (newVal) {
+    await projectStateRefresh()
+  }
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      openCreateStateModal.value = false
+    }
+  })
+})
+
+onMounted(async () => {
+  await handleFetch()
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      openCreateStateModal.value = false
+      resetForm()
+    }
+  })
+})
+
+
+const handleUpdate = async () => {
+  const res = await useCustomFetch(`projects/${props.project.id}/users/`, {
+    method: 'POST',
+    body: JSON.stringify(data)
+  })
+  if (res.error.value === null) {
+    showModal.value = false
+    resetForm()
+  }
+}
+
+</script>
+
+<template>
+  <div :class="{'is-active': showModal}" class="modal settings">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <CreateStateModal :project="project" v-model="openCreateStateModal"/>
+      <div class="box user-box">
+        <h3>Project Settings</h3>
+        <div class="content">
+          <div class="tabs-left">
+            <div class="tabs-titles">
+              <div class="tabs-title" :class="{'is-active': tabs === 'users'}" @click="tabs = 'users'">Users</div>
+              <div class="tabs-title" :class="{'is-active': tabs === 'workflow'}" @click="tabs = 'workflow'">Workflow</div>
+            </div>
+          </div>
+          <div v-if="tabs === 'users'" class="tabs-right user">
+            <div class="field">
+              <div class="select is-multiple">
+                <select multiple size="4" v-if="users.length > 0" v-model="data.users">
+                  <option v-for="user in users.filter(u => u.id !== project.creator)" :key="user.id" :value="user.id">{{ user.username }}</option>
+                </select>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <button class="button" @click="showModal=false; resetForm()" type="button">Cancel</button>
+                <button class="button is-dark" type="submit" @click="handleUpdate">Add</button>
+              </div>
+            </div>
+          </div>
+          <div v-if="tabs === 'workflow'" class="tabs-right workflow">
+            <div class="state-wrapper">
+              <div class="state-infos" v-for="state in projectState" key="state.name">
+                <div class="state-name">
+                  {{ useCapitalize(state.name) }}
+                </div>
+                <div v-if="!state.isdefault" class="state-options">
+                  <div class="edit-icon">
+                    <font-awesome-icon icon="fa-regular fa-pen"/>
+                  </div>
+                  <div class="trash-icon">
+                    <font-awesome-icon icon="fa-solid fa-trash" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="button is-small" @click="openCreateStateModal = true">
+              <span class="icon is-small">
+                <font-awesome-icon icon="fa-solid fa-plus" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <button class="button" @click="showModal=false">Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
