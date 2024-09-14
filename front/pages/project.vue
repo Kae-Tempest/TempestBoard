7be024829441fa7122c86e39bfb1b692a69b0d7a
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-09-14 19:13:45 +0200

    [Front] Move user add and State add to one modal for project with some little fix

--- front/pages/project.vue
+++ front/pages/project.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
 import {onMounted, ref, watch} from 'vue'
-import {useUserStore} from "~/stores/useUserStore";
 import type {Project} from "~/types/global";
 import {useCustomFetch} from "~/composables/useCustomFetch";
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import DeleteProjectModal from "~/components/Modals/Project/DeleteProjectModal.vue";
 import CreateProjectModal from "~/components/Modals/Project/CreateProjectModal.vue";
+import {useUserStore} from "~/stores/useUserStore";
 import EditProjectModal from "~/components/Modals/Project/EditProjectModal.vue";
-import UserProjectModal from "~/components/Modals/Project/UserProjectModal.vue";
+import DeleteProjectModal from "~/components/Modals/Project/DeleteProjectModal.vue";
+import ProjectSettingsModal from "~/components/Modals/Project/ProjectSettingsModal.vue";
 
 useHead({title: 'Project - Tempest Board'})
 
 const user = useUserStore().getUser();
 const projects = ref<Project[]>([]);
 const {data, refresh} = await useCustomFetch<Project[]>('/projects/', {immediate: false})
-const selectedProject = ref<Project | null>(null)
-const showEditModal = ref(false)
-const showDeleteModal = ref(false)
+
+
 const showCreateModal = ref(false)
-const showUserModal = ref(false)
+const showEditModal = ref<Boolean>(false)
+const showDeleteModal = ref<Boolean>(false)
+const showSettingsModal = ref<Boolean>(false)
+const selectedProject = ref<Project | null>(null)
+
+
 const {isRefresh} = useRefreshData()
 
 onMounted(async () => {
@@ -33,21 +36,6 @@ watch(() => isRefresh.value, async (newVal) => {
     isRefresh.value = false
   }
 });
-
-const handleOpenEditModal = (project: Project) => {
-  selectedProject.value = project
-  showEditModal.value = true
-}
-
-const handleOpenDeleteModal = (project: Project) => {
-  selectedProject.value = project
-  showDeleteModal.value = true
-}
-
-const handleOpenUserModal = (project: Project) => {
-  selectedProject.value = project
-  showUserModal.value = true
-}
 
 </script>
 <template>
@@ -67,44 +55,10 @@ const handleOpenUserModal = (project: Project) => {
         <CreateProjectModal v-model="showCreateModal"/>
         <DeleteProjectModal v-if="selectedProject" :project="selectedProject" v-model="showDeleteModal"/>
         <EditProjectModal v-if="selectedProject" :project="selectedProject" v-model="showEditModal"/>
-        <UserProjectModal v-if="selectedProject" :project="selectedProject" v-model="showUserModal"/>
+        <ProjectSettingsModal v-if="selectedProject" :project="selectedProject" v-model="showSettingsModal"/>
         <!--   Project List   -->
         <div v-for="project in projects.sort((p, p2) => p.id - p2.id)">
-          <div class="card">
-            <div class="card-content">
-              <div class="media">
-                <img v-if="project?.thumbnail" :src="project.thumbnail" alt="Placeholder image"/>
-                <div v-else></div>
-              </div>
-              <div class="content">
-                <h2>{{ project.name }}</h2>
-                <p>{{ project.description }}</p>
-                <div class="content-footer">
-                  <div v-if="user?.id == project.creator" class="btn-action">
-                    <button class="button" @click="handleOpenDeleteModal(project)">
-                      <span class="icon is-small">
-                        <font-awesome-icon icon="fa-solid fa-trash-can"/>
-                      </span>
-                    </button>
-                    <button class="button" @click="handleOpenEditModal(project)">
-                      <span class="icon is-small">
-                        <font-awesome-icon icon="fa-solid fa-pen"/>
-                      </span>
-                    </button>
-                    <button class="button" @click="handleOpenUserModal(project)">
-                      <span class="icon is-small">
-                      <font-awesome-icon icon="fa-solid fa-plus"/>
-                      </span>
-                    </button>
-                  </div>
-                  <div class="date">
-                    <time dateTime="2016-1-1">Created: {{ new Date(project.created_at).toLocaleString('en-GB', {day: 'numeric', month: "short", year: "numeric"}) }}</time>
-                    <time dateTime="2016-1-1">Updated: {{ new Date(project.updated_at).toLocaleString('en-GB', {day: 'numeric', month: "short", year: "numeric"}) }}</time>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <CardProjectCard :project="project" :user="user" v-model:edit="showEditModal" v-model:delete="showDeleteModal" v-model:settings="showSettingsModal" v-model:selectedProject="selectedProject"/>
         </div>
       </div>
     </div>
