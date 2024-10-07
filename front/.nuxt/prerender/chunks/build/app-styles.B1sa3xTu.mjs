104be6db62d5c124dfa4a4852bcaf8f92b6d9cea
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-10-07 22:54:32 +0200

    [Front] remove .nuxt fron git

--- front/.nuxt/prerender/chunks/build/app-styles.B1sa3xTu.mjs
+++
@@ -1,6 +0,0 @@
-import { t as toastify } from './entry-styles-5.mjs-Dt9gSd3M.mjs';
-
-const appStyles_B1sa3xTu = [toastify];
-
-export { appStyles_B1sa3xTu as default };
-//# sourceMappingURL=app-styles.B1sa3xTu.mjs.map
--- front/.nuxt/prerender/chunks/build/error-500-styles.DqmriZ_b.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error500_vue_vue_type_style_index_0_scoped_f7ad9679_lang } from './error-500-styles-1.mjs-DiWYPNRc.mjs';
-
-const error500Styles_DqmriZ_b = [error500_vue_vue_type_style_index_0_scoped_f7ad9679_lang];
-
-export { error500Styles_DqmriZ_b as default };
-//# sourceMappingURL=error-500-styles.DqmriZ_b.mjs.map
