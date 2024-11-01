e94553c9c7778aea609129ec9642b3009ba5bcf2
Quentin Takeda <quentin.vadon@gmail.com>
2024-11-01 16:55:54 +0100

    Fixes debug settings variable

--- back/TempestBoard/settings.py
+++ back/TempestBoard/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG'] == "True"
 
 # Application definition
 
