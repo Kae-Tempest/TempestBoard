41837a47f5b0ded621c7e3e49c8dc3e396b289b6
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-11-12 00:03:17 +0100

    [front] feat: Add Profile Modal with Transfer Modal when user delete is account and Change password modal

---
+++ front/components/Modals/user/ChangePasswordModal.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import type {User} from "~/types/global";
+
+interface Props {
+  user: User
+}
+
+const props = defineProps<Props>()
+const showModal = defineModel()
+
+const data = reactive({
+  old_password: "",
+  new_password: "",
+  new_password_confirm: ""
+})
+
+const handleClose = () => {
+  showModal.value = false
+  data.old_password = ""
+  data.new_password = ""
+  data.new_password_confirm = ""
+}
+
+const handleChangePassword = async () => {
+  const res = await useCustomFetch(`/users/${props.user.id}/change_password/`,{
+    method: "POST",
+    body: data
+  })
+  if(res.data.value) {
+    handleClose()
+  } else if (res.error.value) {
+    console.log(res.error.value)
+    // toast ?
+  }
+}
+
+</script>
+
+<template>
+  <div class="modal" :class="{'is-active': showModal}">
+    <div class="modal-background" @click="handleClose"></div>
+    <div class="modal-content">
+      <div class="box">
+        <h2 class="title is-2 title">Change Password !</h2>
+        <div class="field">
+          <div class="control">
+            <label for="old_password">Old Password</label>
+            <input type="password" id="old_password" class="input" v-model="data.old_password">
+            <label for="new_password">New Password</label>
+            <input type="password" class="input" id="new_password" v-model="data.new_password">
+            <label for="new_password_confirm">Confirm New Password</label>
+            <input type="password" class="input" id="new_password_confirm" v-model="data.new_password_confirm">
+          </div>
+          <div class="action_button">
+            <button class="button" @click="handleClose">Cancel</button>
+            <button class="button is-dark" @click="handleChangePassword">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ front/components/Modals/user/TransferOwnerProjectModal.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import type {Project, User} from "~/types/global";
+import {useCustomFetch} from "~/composables/useCustomFetch";
+
+interface Props {
+  project: Project | null
+}
+
+const props = defineProps<Props>()
+const showModal = defineModel()
+const isTransferred = defineModel('isTransferred')
+const {data: userList, refresh: userRefresh } = await useCustomFetch<User[]>(`/users/`)
+const connected_user = useUserStore().getUser
+
+const data = reactive({
+  creator: 0
+})
+
+onMounted(async () => {
+  await userRefresh()
+})
+
+const handleCancel = () => {
+  showModal.value = false
+}
+
+const handleTransfer = async () => {
+  if(!props.project) return
+  const res = await useCustomFetch(`/projects/${props.project.id}/`, {
+    method: "patch",
+    body: data
+  })
+  if(res.data.value) {
+    data.creator = 0
+    isTransferred.value = true
+  } else if (res.error.value) {
+    console.log(res.error.value)
+    // toast ?
+  }
+
+}
+
+</script>
+
+<template>
+    <div class="modal" :class="{'is-active': showModal}">
+      <div class="modal-background" @click="handleCancel"></div>
+      <div class="modal-content">
+        <div class="box">
+          <h2>Owner Transfer - {{ project?.name }}</h2>
+          <div class="select">
+            <select v-if="userList" v-model="data.creator">
+              <option :value="user" v-for="user in project?.users.filter(u => u !== connected_user?.id)">{{ userList.find(u => u.id === user)?.username }}</option>
+            </select>
+          </div>
+          <div class="actions">
+            <button class="button" @click="handleCancel">Cancel</button>
+            <button class="button is-dark" @click="handleTransfer()">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
