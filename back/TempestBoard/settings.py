bed8afe9e3a0b5107363c9c359450925ac01614a
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-11-12 21:23:37 +0100

    [back] feat: Reset Password Gestion send email and change password

--- back/TempestBoard/settings.py
+++ back/TempestBoard/settings.py
@@ -185,3 +185,13 @@ CHANNEL_LAYERS = {
         }
     }
 }
+
+# settings.py additions
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'katsuhiko.tempest@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PWD']
+DEFAULT_FROM_EMAIL = 'noreply@tempestboard.gloupi.com'
+FRONTEND_URL = 'http://localhost:3000'  # Your frontend URL
