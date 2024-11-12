1398ed5d0d71978ab1e73fa2cbc4ef4c95f9797f
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-11-12 21:24:07 +0100

    [front] feat: Add ResetPassword Modal and Reset Password Page

---
+++ front/pages/reset-password/[uid]/[token].vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const dataPwd = reactive({
+  password:"",
+  confirm_password:"",
+  uid: route.params.uid,
+  token: route.params.token
+})
+
+const handleResetPassword = async () => {
+  const {data, error} = await useCustomFetch(`/users/password/reset/`, {
+    method: "POST",
+    body: dataPwd
+  })
+
+  if(data) {
+    console.log(data)
+  } else if (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+  <div id="Auth">
+    <div>
+      <div id="reset-password">
+        <div>
+          <label for="">New Password</label>
+          <input type="password" class="input" v-model="dataPwd.password">
+        </div>
+        <div>
+          <label for="">Confirm Password</label>
+          <input type="password" class="input" v-model="dataPwd.confirm_password">
+        </div>
+      <div class="actions">
+        <button class="button" @click="handleResetPassword">Change Password</button>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
