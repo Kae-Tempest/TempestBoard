f9b042d81bfb6ea66451589a9a2dc9811fc3be8a
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2025-03-26 00:43:45 +0100

    Remove unused

--- frontend/src/interfaces/auth.interface.ts
+++ frontend/src/interfaces/auth.interface.ts
@@ -9,16 +9,3 @@ export interface registerForm {
 	password: string;
 	confirm_password: string;
 }
-
-export interface authResponse {
-	token: string;
-	type: string;
-	id: number;
-}
-
-export interface tokenPayload {
-	userId: number;
-	sub: string;
-	iat: number;
-	exp: number;
-}
