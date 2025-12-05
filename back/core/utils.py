9a7d4b87934f070cb5c2f0229deabc710506d0d8
Kae <kae.tempest@gloupi.com>
2025-12-06 00:29:04 +0100

    V2 Begin

--- back/core/utils.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.core.files.uploadedfile import UploadedFile
-
-def checkfilesize(file: UploadedFile):
-    if file.size > 3 * 1024 * 1024:
-        raise ValidationError(
-            "%(value)s is to big",
-            params={"value": file.size},
-        )
